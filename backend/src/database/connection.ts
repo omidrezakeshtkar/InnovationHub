35ca4abbcdb1754532d12fcc5bdde858abc9dd64
omid reza keshtkar <omidrezakeshtkar@outlook.com>
2024-10-13 00:25:42 +0330

    feat: update connection database

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,23 +1,25 @@
-import mongoose from 'mongoose';
-import config from '../config';
+import mongoose from "mongoose";
+import config from "../config";
 
 const MAX_RETRIES = 5;
 const RETRY_INTERVAL = 5000; // 5 seconds
 
 export const connectDatabase = async (retryCount = 0) => {
-  try {
-    console.log('Attempting to connect to database...');
-    mongoose.set('strictQuery', true);
-    await mongoose.connect(config.databaseUrl);
-    console.log('Connected to database');
-  } catch (error) {
-    console.error('Database connection error:', error);
-    if (retryCount < MAX_RETRIES) {
-      console.log(`Retrying in ${RETRY_INTERVAL / 1000} seconds...`);
-      setTimeout(() => connectDatabase(retryCount + 1), RETRY_INTERVAL);
-    } else {
-      console.error('Max retries reached. Exiting...');
-      process.exit(1);
-    }
-  }
+	try {
+		console.log("Attempting to connect to database...");
+		mongoose.set("strictQuery", true);
+		await mongoose.connect(config.databaseUrl);
+		console.log("Connected to database");
+	} catch (error) {
+		console.error("Database connection error:", error);
+		if (retryCount < MAX_RETRIES) {
+			console.log(`Retrying in ${String(RETRY_INTERVAL / 1000)} seconds...`);
+			setTimeout(() => {
+				void connectDatabase(retryCount + 1);
+			}, RETRY_INTERVAL);
+		} else {
+			console.error("Max retries reached. Exiting...");
+			process.exit(1);
+		}
+	}
 };
